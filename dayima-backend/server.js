8cc2c85fa4e17a986577f4388298b8eba1e96ae6
Lemonteamaomao <victoriakjx@gmail.com>
2026-03-08 19:03:40 +0800

    Update server.js

--- dayima-backend/server.js
+++ dayima-backend/server.js
@@ -133,4 +133,5 @@ app.delete("/api/book-hygiene/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("🚀 Server running on Port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on Port ${PORT}`));
